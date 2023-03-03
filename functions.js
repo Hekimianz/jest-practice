8389021e9491622894fe1fa0741269efdb812e0f
Aram Hekimian <aramhek121@gmail.com>
2023-03-02 22:04:57 -0600

    finished implementing all tests

--- functions.js
+++ functions.js
@@ -1,5 +1,112 @@
-function sum(a, b) {
-  return a + b;
+function capitalize(string) {
+  const string2 = string.charAt(0).toUpperCase() + string.slice(1);
+  return string2;
 }
 
-export default sum;
+function reverseString(string) {
+  const string2 = string.split("").reverse().join("");
+  return string2;
+}
+
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
+const alphabet = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+};
+
+function shiftedNum(string) {
+  const regExp = /\p{P}/gu;
+  const splitString = string.toLowerCase().split("");
+  const numString = [];
+  splitString.forEach((letter) => {
+    if (letter === " " || regExp.test(letter)) {
+      numString.push(letter);
+      return;
+    }
+
+    if (letter === "z") {
+      numString.push(1);
+    }
+    let shifted = [];
+    shifted = alphabet[letter] + 1;
+    numString.push(shifted);
+  });
+  return numString;
+}
+
+function shiftedWord(num) {
+  const regExp = /\p{P}/gu;
+  const stringArr = [];
+  num.forEach((number) => {
+    if (number === " " || regExp.test(number)) {
+      stringArr.push(number);
+      return;
+    }
+    if (number !== " ") {
+      stringArr.push(Object.keys(alphabet)[number - 1]);
+    }
+  });
+  const result = stringArr.join("");
+  return result;
+}
+
+function ceasarCypher(string) {
+  const num = shiftedNum(string);
+  const newString = shiftedWord(num);
+  return newString;
+}
+
+function analyzeArray(arr) {
+  const arrSum = arr.reduce((acc, current) => acc + current);
+  const arrAvg = arrSum / arr.length;
+  const arrMax = Math.max(...arr);
+  const arrMin = Math.min(...arr);
+
+  const obj = {
+    average: arrAvg,
+    min: arrMin,
+    max: arrMax,
+    length: arr.length,
+  };
+  return obj;
+}
+
+export { capitalize, reverseString, calculator, ceasarCypher, analyzeArray };
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,81 @@
-import sum from "./functions.js";
+/* eslint-disable no-undef */
+import {
+  reverseString,
+  capitalize,
+  calculator,
+  ceasarCypher,
+  analyzeArray,
+} from "./functions";
 
-test("1 + 2 = 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("capitalize: one letter string", () => {
+  expect(capitalize("hello")).toMatch("Hello");
+});
+
+test("capitalize: multi letter string", () => {
+  expect(capitalize("hello world!")).toMatch("Hello world!");
+});
+
+test("reverse: one letter string", () => {
+  expect(reverseString("hello")).toMatch("olleh");
+});
+
+test("reverse: two letter string", () => {
+  expect(reverseString("hello world!")).toMatch("!dlrow olleh");
+});
+
+test("calculator: add two positive numbers", () => {
+  expect(calculator.add(10, 20)).toBe(30);
+});
+
+test("calculator: add two negative numbers", () => {
+  expect(calculator.add(-20, 10)).toBe(-10);
+});
+
+test("calculator: subtract two positive numbers", () => {
+  expect(calculator.subtract(10, 5)).toBe(5);
+});
+
+test("calculator: subtract two negative numbers", () => {
+  expect(calculator.subtract(-10, -5)).toBe(-5);
+});
+
+test("calculator: divide with positive numbers", () => {
+  expect(calculator.divide(200, 2)).toBe(100);
+});
+
+test("calculator: divide with negative numbers", () => {
+  expect(calculator.divide(-200, 2)).toBe(-100);
+});
+
+test("calculator: muliply with positive numbers", () => {
+  expect(calculator.multiply(5, 3)).toBe(15);
+});
+
+test("calculator: muliply with negative numbers", () => {
+  expect(calculator.multiply(5, -3)).toBe(-15);
+});
+
+test("ceasar cipher: one letter string", () => {
+  expect(ceasarCypher("hello")).toBe("ifmmp");
+});
+
+test("ceasar cipher: multi letter string", () => {
+  expect(ceasarCypher("hello world")).toBe("ifmmp xpsme");
+});
+
+test("ceasar cipher: ignore punctuation", () => {
+  expect(ceasarCypher("hello, world!")).toBe("ifmmp, xpsme!");
+});
+
+test("ceasar cipher: not case sensitive", () => {
+  expect(ceasarCypher("Hello, World!")).toBe("ifmmp, xpsme!");
+});
+
+test("analyzeArray: return correct data", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
